1cec7d3b2727cbae479d98a1cccbe3d307081999
Moisès Gómez <iesmoises@gmail.com>
2012-06-21 16:17:03 +0200

    starting typemore branch

---
+++ typemore.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# encoding: utf-8
+#
+#
+# Programa que permet practicar mecanografia
+# Recull paraules d'un fitxer, en construeix frases (no
+# necessàriament amb sentit) i les demana fins que 
+# l'usuari és capaç d'escriure cada paraula
+# prou vegades sense error.
+#
+# Les paraules són agrupades segons un pes. El pes es calcula a
+# partir d'un algorisme que considera, entre altres elements, les
+# els errors de l'usuari en teclejar els caracters que la composen.
+#
+# Aquesta versió és monousuari. Per tant, cada cop que es faci 
+# servir, els pesos es calcularan en funció de l'usuari anterior.
+#
+# Requereix rebre per paràmetre el nom d'un fitxer accessible per 
+# lectura amb les paraules.
+
+#
+#
+def main():
+    pass
+#
+if __name__=="__main__":
+    sys.exit(main())
+
